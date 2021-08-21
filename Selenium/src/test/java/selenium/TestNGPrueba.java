e884a221d0c93a932723bd62a310f34dba1e2f03
Esteban Oscar Ferreira <ferreira_eo@hotmail.com>
2021-08-21 13:10:13 -0300

    First commit

---
+++ Selenium/src/test/java/selenium/TestNGPrueba.java
@@ -0,0 +1,16 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestNGPrueba {
+  @Test
+  public void f() {
+	  String exePath = "./driver/chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", exePath);
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		driver.close();
+  }
+}
